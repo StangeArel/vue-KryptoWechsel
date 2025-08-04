0608220a060edd7ab5688682de15eef00a3cbda1
Valeriya Stange <valeriya.stange@gmail.com>
2025-08-04 23:42:57 +0200

    🎨 Erstes UI-Setup: Input-Komponente, Schriftarten & Grundlayout implementiert
    - Neue Input-Komponente erstellt mit Zahleneingabe und Platzhalter
    - App.vue angepasst: Überschrift & Input-Komponente integriert
    - style.css: Nabla und Montserrat als Schriftarten eingebunden
    - Hintergrund mit linearem Farbverlauf gestaltet
    - Schrift- und Layout-Styling für Body und h1 hinzugefügt
    - index.html aktualisiert (Favicon, Titel, main.js-Verlinkung)
    - main.js lädt App.vue und globale Styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
-<template></template>
+<template>
+  <h1>vue-KryptoWechsel</h1>
+  <Input />
+</template>
 
-<script></script>
+<script>
+import Input from './components/Input.vue'
+
+export default {
+  components: {
+    Input,
+  },
+}
+</script>
 
 <style scoped></style>
---
+++ src/components/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="number" placeholder="Bitte eine Zahl eingeben …" />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+input {
+  border-radius: 3px;
+  border: 0;
+  padding: 20px 25px;
+  background: #fafafa;
+  color: #333;
+  outline: none;
+  width: 500px;
+  position: relative;
+  top: -20px;
+  font-size: 1.2em;
+  margin: 10px;
+}
+</style>
