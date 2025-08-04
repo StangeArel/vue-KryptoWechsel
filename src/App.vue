3d00a4b2648762442bb2faf1299c873422841ebc
Valeriya Stange <valeriya.stange@gmail.com>
2025-08-05 00:03:44 +0200

    🎨 Komponente 'Selector' erstellt und gestylt
    - Neue Vue-Komponente zur Auswahl von Kryptowährungen implementiert;
    - Komponente in der Hauptansicht eingebunden und optisch sortiert

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <h1>vue-KryptoWechsel</h1>
   <Input />
+  <div className="selectors">
+    <Selector />
+    <Selector />
+  </div>
 </template>
 
 <script>
 import Input from './components/Input.vue'
+import Selector from './components/Selector.vue'
 
 export default {
-  components: {
-    Input,
-  },
+  components: { Input, Selector },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.selectors {
+  display: flex;
+  justify-content: space-around;
+  width: 700px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Selector.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <li>Bitcoin</li>
+    <li>ETH</li>
+    <li>USDT</li>
+  </ul>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  width: 200px;
+  padding: 0;
+  border-radius: 3px;
+  border: 3px solid #000;
+}
+
+LI {
+  display: block;
+  background: #1a032d;
+  color: #fff;
+  padding: 20px 0;
+}
+
+li:hover,
+li.active {
+  background: #3a0f4d;
+  cursor: pointer;
+  border-radius: 50% 0 50% 0;
+}
+</style>
